e329a208d964346328ff1a4b05c5e2f24f24fb83
foxymen9 <foxymen9@gmail.com>
2017-12-22 06:55:52 +0200

    create files helper

--- djangorest/api/helpers.py
+++ djangorest/api/helpers.py
@@ -1,5 +1,10 @@
 
 import os, errno
+from datetime import datetime
+from django.conf import settings
+
+OUT_DIR = settings.BASE_DIR + '/../files'
+
 
 def create_folder(path):
   try:
@@ -7,6 +12,36 @@ def create_folder(path):
   except OSError as e:
     if e.errno != errno.EEXIST:
       raise
+
+
+def create_file(file_name, stats):
+  now = datetime.now()
+  date = now.strftime("%m-%d-%Y")
+  time = now.strftime("%m-%d-%Y-%H-%M-%S")
+  
+  path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
+  path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
+
+  create_folder(path_latest)
+  create_folder(path_archived)
+
+  df = stats.overall()
+  df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
+  df.to_json(path_latest + file_name + '.json', orient='records')
+  df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
+  df.to_json(path_archived + file_name + '.json', orient='records')
+
+
+def get_last_games():
+  return [
+    { 'name': 'Season', 'value': 0 },
+    { 'name': 'L1', 'value': 1 },
+    { 'name': 'L2', 'value': 2 },
+    { 'name': 'L3', 'value': 3 },
+    { 'name': 'L5', 'value': 5 },
+    { 'name': 'L10', 'value': 10 }
+  ]
+
 
 def get_categories(type, section):
   if(section == 'General'):
@@ -96,12 +131,3 @@ def get_categories(type, section):
   
   return []
 
-def get_last_games(type, section):
-  return [
-    { 'name': 'Season', 'value': 0 },
-    { 'name': 'L1', 'value': 1 },
-    { 'name': 'L2', 'value': 2 },
-    { 'name': 'L3', 'value': 3 },
-    { 'name': 'L5', 'value': 5 },
-    { 'name': 'L10', 'value': 10 }
-  ]
--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -1,11 +1,9 @@
 from datetime import datetime
-from django.conf import settings
 from rest_framework import generics
 from rest_framework.response import Response
 from nba_py import team, league
 from . import helpers
 
-OUT_DIR = settings.BASE_DIR + '/../files'
 
 class NBAStatsView(generics.ListAPIView):
 
@@ -21,6 +19,7 @@ class NBAStatsView(generics.ListAPIView):
     teamShooting = TeamShooting()
     teamOpponentShooting = TeamOpponentShooting()
     teamHustle = TeamHustle()
+    playerGeneral = PlayerGeneral()
 
     teamGeneral.create_files()
     teamClutch.create_files()
@@ -33,336 +32,188 @@ class NBAStatsView(generics.ListAPIView):
     teamShooting.create_files()
     teamOpponentShooting.create_files()
     teamHustle.create_files()
+    # playerGeneral.create_file()
 
     return Response("OK", 200)
 
 class TeamGeneral():
 
-  def create_file(self, category, last_game):
-    now = datetime.now()
-    date = now.strftime("%m-%d-%Y")
-    time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    
-    file_name = 'team_general_' + category['key'] + '_' + last_game['name']
-    path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
-    path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
-
-    helpers.create_folder(path_latest)
-    helpers.create_folder(path_archived)
-
-    stats = league.TeamStats(measure_type=category['name'], last_n_games=last_game['value'])
-    df = stats.overall()
-    df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_latest + file_name + '.json', orient='records')
-    df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_archived + file_name + '.json', orient='records')
-
   def create_files(self):
     categories = helpers.get_categories('team', 'General')
-    last_games = helpers.get_last_games('team', 'General')
+    last_games = helpers.get_last_games()
 
     for category in categories:
       for last_game in last_games:
-        self.create_file(category, last_game)
+        file_name = 'team_general_' + category['key'] + '_' + last_game['name']
+        stats = league.TeamStats(measure_type=category['name'], last_n_games=last_game['value'])
+
+        helpers.create_file(file_name, stats)
+
+
+class PlayerGeneral():
+
+  def create_files(self):
+    categories = helpers.get_categories('player', 'General')
+    last_games = helpers.get_last_games()
+
+    for category in categories:
+      for last_game in last_games:
+        file_name = 'player_general_' + category['key'] + '_' + last_game['name']
+        stats = league.TeamStats(measure_type=category['name'], last_n_games=last_game['value'])
+        
+        helpers.create_file(file_name, stats)
+
 
 class TeamClutch():
 
-  def create_file(self, category, last_game):
-    now = datetime.now()
-    date = now.strftime("%m-%d-%Y")
-    time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    
-    file_name = 'team_clutch_' + category['key'] + '_' + last_game['name']
-    path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
-    path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
-
-    helpers.create_folder(path_latest)
-    helpers.create_folder(path_archived)
-
-    stats = league.TeamClutch(measure_type=category['name'], last_n_games=last_game['value'])
-    df = stats.overall()
-    df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_latest + file_name + '.json', orient='records')
-    df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_archived + file_name + '.json', orient='records')
-
   def create_files(self):
     categories = helpers.get_categories('team', 'Clutch')
-    last_games = helpers.get_last_games('team', 'Clutch')
+    last_games = helpers.get_last_games()
 
     for category in categories:
       for last_game in last_games:
-        self.create_file(category, last_game)
+        file_name = 'team_clutch_' + category['key'] + '_' + last_game['name']
+        stats = league.TeamClutch(measure_type=category['name'], last_n_games=last_game['value'])
+
+        helpers.create_file(file_name, stats)
+
 
 class TeamPlaytype():
 
-  def create_file(self, category):
-    now = datetime.now()
-    date = now.strftime("%m-%d-%Y")
-    time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    
-    file_name = 'team_playtype_' + category['key']
-    path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
-    path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
-
-    helpers.create_folder(path_latest)
-    helpers.create_folder(path_archived)
-
-    stats = league.TeamPlaytype(category=category['name'], season=now.year)
-    df = stats.overall()
-    df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_latest + file_name + '.json', orient='records')
-    df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_archived + file_name + '.json', orient='records')
-
   def create_files(self):
+    now = datetime.now()
     categories = helpers.get_categories('team', 'Playtype')
 
     for category in categories:
-      self.create_file(category)
+      file_name = 'team_playtype_' + category['key']
+      stats = league.TeamPlaytype(category=category['name'], season=now.year)
+
+      helpers.create_file(file_name, stats)
+
 
 class TeamTracking():
-  
-  def create_file(self, category, last_game):
-    now = datetime.now()
-    date = now.strftime("%m-%d-%Y")
-    time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    
-    file_name = 'team_tracking_' + category['key'] + '_' + last_game['name']
-    path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
-    path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
-
-    helpers.create_folder(path_latest)
-    helpers.create_folder(path_archived)
-
-    stats = league._PlayerTrackingStats(player_or_team='Team', pt_measure_type=category['name'], last_n_games=last_game['value'])
-    df = stats.overall()
-    df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_latest + file_name + '.json', orient='records')
-    df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_archived + file_name + '.json', orient='records')
 
   def create_files(self):
     categories = helpers.get_categories('team', 'Tracking')
-    last_games = helpers.get_last_games('team', 'Tracking')
+    last_games = helpers.get_last_games()
 
     for category in categories:
       for last_game in last_games:
-        self.create_file(category, last_game)
+        file_name = 'team_tracking_' + category['key'] + '_' + last_game['name']
+        stats = league._PlayerTrackingStats(player_or_team='Team', pt_measure_type=category['name'], last_n_games=last_game['value'])
+
+        helpers.create_file(file_name, stats)
+
 
 class TeamDefenseDashboard():
-  
-  def create_file(self, category, last_game):
-    now = datetime.now()
-    date = now.strftime("%m-%d-%Y")
-    time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    
-    file_name = 'team_defensedashboard_' + category['key'] + '_' + last_game['name']
-    path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
-    path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
-
-    helpers.create_folder(path_latest)
-    helpers.create_folder(path_archived)
-
-    stats = league.TeamDefenseDashboard(defense_category=category['name'], last_n_games=last_game['value'])
-    df = stats.overall()
-    df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_latest + file_name + '.json', orient='records')
-    df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_archived + file_name + '.json', orient='records')
 
   def create_files(self):
     categories = helpers.get_categories('team', 'DefenseDashboard')
-    last_games = helpers.get_last_games('team', 'DefenseDashboard')
+    last_games = helpers.get_last_games()
 
     for category in categories:
       for last_game in last_games:
-        self.create_file(category, last_game)
+        file_name = 'team_defensedashboard_' + category['key'] + '_' + last_game['name']
+        stats = league.TeamDefenseDashboard(defense_category=category['name'], last_n_games=last_game['value'])
+
+        helpers.create_file(file_name, stats)
+
 
 class TeamShotDashboard():
-  
-  def create_file(self, category, last_game):
-    now = datetime.now()
-    date = now.strftime("%m-%d-%Y")
-    time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    
-    file_name = 'team_shotdashboard_' + category['key'] + '_' + last_game['name']
-    path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
-    path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
-
-    helpers.create_folder(path_latest)
-    helpers.create_folder(path_archived)
-
-    stats = None
-
-    if(category['key'] == 'general'):
-      stats = league.TeamShotDashboard(general_range=category['name'], last_n_games=last_game['value'])
-    elif(category['key'] == 'shotclock'):
-      stats = league.TeamShotDashboard(shotclock_range=category['name'], last_n_games=last_game['value'])
-    elif(category['key'] == 'dribbles'):
-      stats = league.TeamShotDashboard(dribbles_range=category['name'], last_n_games=last_game['value'])
-    elif(category['key'] == 'touchtime'):
-      stats = league.TeamShotDashboard(touchtime_range=category['name'], last_n_games=last_game['value'])
-    elif(category['key'] == 'closestdefender'):
-      stats = league.TeamShotDashboard(closedefdist_range=category['name'], last_n_games=last_game['value'])
-    elif(category['key'] == 'closestdefender10p'):
-      stats = league.TeamShotDashboard(closedefdist_range=category['name'], shotdist_range='>=10.0', last_n_games=last_game['value'])
-    
-    if(stats != None):
-      df = stats.overall()
-      df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
-      df.to_json(path_latest + file_name + '.json', orient='records')
-      df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
-      df.to_json(path_archived + file_name + '.json', orient='records')
 
   def create_files(self):
     categories = helpers.get_categories('team', 'ShotDashboard')
-    last_games = helpers.get_last_games('team', 'ShotDashboard')
+    last_games = helpers.get_last_games()
 
     for category in categories:
       for last_game in last_games:
-        self.create_file(category, last_game)
+        file_name = 'team_shotdashboard_' + category['key'] + '_' + last_game['name']
+        stats = None
+
+        if(category['key'] == 'general'):
+          stats = league.TeamShotDashboard(general_range=category['name'], last_n_games=last_game['value'])
+        elif(category['key'] == 'shotclock'):
+          stats = league.TeamShotDashboard(shotclock_range=category['name'], last_n_games=last_game['value'])
+        elif(category['key'] == 'dribbles'):
+          stats = league.TeamShotDashboard(dribbles_range=category['name'], last_n_games=last_game['value'])
+        elif(category['key'] == 'touchtime'):
+          stats = league.TeamShotDashboard(touchtime_range=category['name'], last_n_games=last_game['value'])
+        elif(category['key'] == 'closestdefender'):
+          stats = league.TeamShotDashboard(closedefdist_range=category['name'], last_n_games=last_game['value'])
+        elif(category['key'] == 'closestdefender10p'):
+          stats = league.TeamShotDashboard(closedefdist_range=category['name'], shotdist_range='>=10.0', last_n_games=last_game['value'])
+        
+        if(stats != None):
+          helpers.create_file(file_name, stats)
+
 
 class BoxScores():
   
   def create_files(self):
-    now = datetime.now()
-    date = now.strftime("%m-%d-%Y")
-    time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    
     file_name = 'team_boxscores'
-    path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
-    path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
-
-    helpers.create_folder(path_latest)
-    helpers.create_folder(path_archived)
-
     stats = league.GameLog()
     
-    df = stats.overall()
-    df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_latest + file_name + '.json', orient='records')
-    df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_archived + file_name + '.json', orient='records')
+    helpers.create_file(file_name, stats)
+
 
 class TeamAdvancedBoxScores():
 
-  def create_file(self, category, last_game):
-    now = datetime.now()
-    date = now.strftime("%m-%d-%Y")
-    time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    
-    file_name = 'team_advancedboxscores_' + category['key'] + '_' + last_game['name']
-    path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
-    path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
-
-    helpers.create_folder(path_latest)
-    helpers.create_folder(path_archived)
-
-    stats = team.TeamGameLogs(measure_type=category['name'], last_n_games=last_game['value'])
-    df = stats.overall()
-    df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_latest + file_name + '.json', orient='records')
-    df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_archived + file_name + '.json', orient='records')
-
   def create_files(self):
     categories = helpers.get_categories('Team', 'AdvancedBoxScores')
-    last_games = helpers.get_last_games('Team', 'AdvancedBoxScores')
+    last_games = helpers.get_last_games()
 
     for category in categories:
       for last_game in last_games:
-        self.create_file(category, last_game)
+        file_name = 'team_advancedboxscores_' + category['key'] + '_' + last_game['name']
+        stats = team.TeamGameLogs(measure_type=category['name'], last_n_games=last_game['value'])
+
+        helpers.create_file(file_name, stats)
+
 
 class TeamShooting():
   
   def create_files(self):
-    now = datetime.now()
-    date = now.strftime("%m-%d-%Y")
-    time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    
     file_name = 'team_shooting'
-    path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
-    path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
-
-    helpers.create_folder(path_latest)
-    helpers.create_folder(path_archived)
-
     stats = league.TeamShooting()
     
-    df = stats.overall()
-    df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_latest + file_name + '.json', orient='records')
-    df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_archived + file_name + '.json', orient='records')
+    helpers.create_file(file_name, stats)
+
 
 class TeamOpponentShooting():
-  
-  def create_file(self, category):
-    now = datetime.now()
-    date = now.strftime("%m-%d-%Y")
-    time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    
-    file_name = 'team_opponentshooting_' + category['key']
-    path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
-    path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
-
-    helpers.create_folder(path_latest)
-    helpers.create_folder(path_archived)
-
-    stats = None
-
-    if(category['key'] == 'opponent'):
-      stats = league.TeamShooting(measure_type=category['name'])
-    elif(category['key'] == 'general'):
-      stats = league.TeamOpponentShooting(general_range=category['name'])
-    elif(category['key'] == 'shotclock'):
-      stats = league.TeamOpponentShooting(shotclock_range=category['name'])
-    elif(category['key'] == 'dribbles'):
-      stats = league.TeamOpponentShooting(dribbles_range=category['name'])
-    elif(category['key'] == 'touchtime'):
-      stats = league.TeamOpponentShooting(touchtime_range=category['name'])
-    elif(category['key'] == 'closestdefender'):
-      stats = league.TeamOpponentShooting(closedefdist_range=category['name'])
-    elif(category['key'] == 'closestdefender10p'):
-      stats = league.TeamOpponentShooting(closedefdist_range=category['name'], shotdist_range='>=10.0')
-    
-    if(stats != None):
-      df = stats.overall()
-      df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
-      df.to_json(path_latest + file_name + '.json', orient='records')
-      df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
-      df.to_json(path_archived + file_name + '.json', orient='records')
 
   def create_files(self):
     categories = helpers.get_categories('Team', 'OpponentShooting')
 
     for category in categories:
-      self.create_file(category)
+      file_name = 'team_opponentshooting_' + category['key']
+      stats = None
+
+      if(category['key'] == 'opponent'):
+        stats = league.TeamShooting(measure_type=category['name'])
+      elif(category['key'] == 'general'):
+        stats = league.TeamOpponentShooting(general_range=category['name'])
+      elif(category['key'] == 'shotclock'):
+        stats = league.TeamOpponentShooting(shotclock_range=category['name'])
+      elif(category['key'] == 'dribbles'):
+        stats = league.TeamOpponentShooting(dribbles_range=category['name'])
+      elif(category['key'] == 'touchtime'):
+        stats = league.TeamOpponentShooting(touchtime_range=category['name'])
+      elif(category['key'] == 'closestdefender'):
+        stats = league.TeamOpponentShooting(closedefdist_range=category['name'])
+      elif(category['key'] == 'closestdefender10p'):
+        stats = league.TeamOpponentShooting(closedefdist_range=category['name'], shotdist_range='>=10.0')
+      
+      if(stats != None):
+        helpers.create_file(file_name, stats)
+
 
 class TeamHustle():
 
-  def create_file(self, last_game):
-    now = datetime.now()
-    date = now.strftime("%m-%d-%Y")
-    time = now.strftime("%m-%d-%Y-%H-%M-%S")
-    
-    file_name = 'team_hustle_' + last_game['name']
-    path_latest = OUT_DIR + '/stats/nba/' + date + '/latest/'
-    path_archived = OUT_DIR + '/stats/nba/' + date + '/archived/' + time + '/'
-
-    helpers.create_folder(path_latest)
-    helpers.create_folder(path_archived)
-
-    stats = league.TeamHustle(last_n_games=last_game['value'])
-    df = stats.overall()
-    df.to_csv(path_latest + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_latest + file_name + '.json', orient='records')
-    df.to_csv(path_archived + file_name + '.csv', encoding='utf-8')
-    df.to_json(path_archived + file_name + '.json', orient='records')
-
   def create_files(self):
-    last_games = helpers.get_last_games('Team', 'Hustle')
+    last_games = helpers.get_last_games()
 
     for last_game in last_games:
-      self.create_file(last_game)
+      file_name = 'team_hustle_' + last_game['name']
+      stats = league.TeamHustle(last_n_games=last_game['value'])
+      
+      helpers.create_file(file_name, stats)
